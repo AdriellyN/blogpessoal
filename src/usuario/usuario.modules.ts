2e8bec66f520c805fdc4e8130c34606871bcf0e1
Adrielly <adriellynr@gmail.com>
2025-01-24 16:02:49 -0300

    :sparkles: Implementação de testes de unidade

--- src/usuario/usuario.modules.ts
+++ src/usuario/usuario.modules.ts
@@ -11,4 +11,4 @@ import { AuthModule } from '../auth/auth.module';
     providers: [UsuarioService],
     exports: [UsuarioService],
 })
-export class UsuarioModule { }
+export class UsuarioModule {}
